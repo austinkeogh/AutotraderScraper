703fbbfeb695792b1ce3cd08cc2cc3a4b53fd2f3
austinkeogh <austin.keogh@gmail.com>
2020-07-27 16:17:29 +0100

    Added CSV output file format

--- auto1.py
+++ auto1.py
@@ -4,34 +4,49 @@ import sys
 import csv
 from bs4 import BeautifulSoup
 from time import gmtime, strftime
+import pandas as pd
 
 urlx="https://www.autotrader.co.uk/car-search?sort=sponsored&radius=1500&postcode=M15%204FN&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=TESLA&model=MODEL%20S&page="
 
 #make = "TESLA"
 #model = "MODEL%20S"
 
-output_file = open("Autotrader scraping.txt",'w')
+text_file = open("Autotrader scraping.txt",'w')
+csv_file = open("Autotrader scraping.csv",'w')
+xls_file = open("Autotrader scraping.xls",'w')
+
+def txt_to_csv():
+    text_file = open("Autotrader scraping.txt", 'r')
+    read_file = pd.read_csv(text_file, delimiter='\n')
+    print(read_file)
+    read_file.to_csv(csv_file)
+    csv_file.close()
+
+def txt_to_xls(): #not working
+    text_file = open("Autotrader scraping.txt", 'r')
+    read_file = pd.read_excel(text_file)
+    print(read_file)
+    read_file.to_excel(xls_file)
+    csv_file.close()
 
 def write_to_file(data):
-    output_file.write(str(data))
+    text_file.write(str(data))
 
 def write_to_csv(csvData): # notfinished
-    #csvData = [['Person', 'Age'], ['Peter', '22'], ['Jasmine', '21'], ['Sam', '24']]
     with open('person.csv', 'w') as csvFile:
-        dialect = csv.excel_tab
         writer = csv.writer(csvFile)
         writer.writerow(csvData)
         print(csvData)
     csvFile.close()
 
 def get_results(url):
-    #req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-    req = urllib.request.FancyURLopener({"http":"http://127.0.0.1:8080"}).open(url).read().decode("utf-8")
-    print(req)
+    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
+    #req = urllib.request.FancyURLopener({"http":"http://127.0.0.1:8080"}).open(url).read().decode("utf-8")
+    #print(req)
     con = urllib.request.urlopen( req )
-    print (con)
+    #print (con)
     html=con.read()
-    print (html)
+    #print (html)
     soup = BeautifulSoup(html, features="html.parser")
     
     try: 
@@ -56,6 +71,8 @@ print ('###### AutoTrader Scraping Tool')
 print ('##########################################################################\n')
 for x in range (1, 15):
     get_results(urlx + str(x))
-output_file.close()
-#txt_to_csv
+print(text_file)
+text_file.close()
+txt_to_csv()
+
 

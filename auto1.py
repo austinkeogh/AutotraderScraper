17316f3047a3a6b04a6292d0b44b576bbd3a92bd
austinkeogh <austin.keogh@gmail.com>
2020-07-27 22:02:08 +0100

    Got pagination parameterised

--- auto1.py
+++ auto1.py
@@ -3,17 +3,38 @@ import urllib.request
 import sys
 import csv
 from bs4 import BeautifulSoup
-from time import gmtime, strftime
+import re
 import pandas as pd
 
 urlx="https://www.autotrader.co.uk/car-search?sort=sponsored&radius=1500&postcode=M15%204FN&onesearchad=Used&onesearchad=Nearly%20New&onesearchad=New&make=TESLA&model=MODEL%20S&page="
-
 #make = "TESLA"
 #model = "MODEL%20S"
 
 text_file = open("Autotrader scraping.txt",'w')
 csv_file = open("Autotrader scraping.csv",'w')
-xls_file = open("Autotrader scraping.xls",'w')
+#xls_file = open("Autotrader scraping.xls",'w')
+
+def bs_setup(url):
+    req = urllib.request.Request(url, headers={'User-Agent': "Magic Browser"})
+    con = urllib.request.urlopen(req)
+    html = con.read()
+    soup = BeautifulSoup(html, features="html.parser")
+    return soup
+
+def get_pages(url):
+    soup = bs_setup(url)
+    try:
+        results = soup.find('h1', attrs={'class': 'search-form__count'})
+        for result in results.children:
+            print(result)
+        pagenumber = soup.find('li', attrs={'class': 'paginationMini__count'})
+        str1 = str((pagenumber.get_text))
+        num_of_pages = re.search('(\d+)(?!.*\d)', str1).group(1)
+        print(num_of_pages)
+    except:
+        print("Error getting the listings")
+        raise
+    return int(num_of_pages)
 
 def txt_to_csv():
     text_file = open("Autotrader scraping.txt", 'r')
@@ -21,37 +42,31 @@ def txt_to_csv():
     print(read_file)
     read_file.to_csv(csv_file)
     csv_file.close()
+    text_file.close()
 
-def txt_to_xls(): #not working
-    text_file = open("Autotrader scraping.txt", 'r')
-    read_file = pd.read_excel(text_file)
-    print(read_file)
-    read_file.to_excel(xls_file)
-    csv_file.close()
+# def txt_to_xls(): #not working
+#     text_file = open("Autotrader scraping.txt", 'r')
+#     read_file = pd.read_excel(text_file)
+#     print(read_file)
+#     read_file.to_excel(xls_file)
+#     csv_file.close()
+#     text_file.close()
 
 def write_to_file(data):
     text_file.write(str(data))
 
 def write_to_csv(csvData): # notfinished
-    with open('person.csv', 'w') as csvFile:
+    with open('test.csv', 'w') as csvFile:
         writer = csv.writer(csvFile)
         writer.writerow(csvData)
         print(csvData)
     csvFile.close()
 
 def get_results(url):
-    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"})
-    #req = urllib.request.FancyURLopener({"http":"http://127.0.0.1:8080"}).open(url).read().decode("utf-8")
-    #print(req)
-    con = urllib.request.urlopen( req )
-    #print (con)
-    html=con.read()
-    #print (html)
-    soup = BeautifulSoup(html, features="html.parser")
-    
-    try: 
-        listings = soup.find_all('div', attrs={'class': 'js-search-results'})
+    soup = bs_setup(url)
 
+    try:
+        listings = soup.find_all('div', attrs={'class': 'js-search-results'})
     except:
         print("Error getting the listings")
         raise
@@ -80,8 +95,11 @@ def get_results(url):
 
 print ('AutoTrader Scraping Tool')
 print ('##########################\n')
-for x in range (1, 15):
+
+pagination_value = get_pages(urlx)
+for x in range (1, pagination_value):
     get_results(urlx + str(x))
+
 print(text_file)
 text_file.close()
 txt_to_csv()
